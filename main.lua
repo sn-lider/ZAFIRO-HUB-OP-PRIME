42af641bf5f756074ebfbeae6a3813c4e9d6a8d1
sn-lider <emilianoasc2@gmail.com>
2026-01-21 23:14:03 -0600

    Update library URL and main color in UI

--- main.lua
+++ main.lua
@@ -11,13 +11,13 @@ end
 Players.LocalPlayer.Idled:Connect(preventAFK)
 print("Anti-AFK ativado!")
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sn-lider/FREE-LIBRARY-OP/refs/heads/main/main.lua", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/sn-lider/COPlibarykitsune/refs/heads/main/main.lua", true))()
 
 local player = game.Players.LocalPlayer
 local displayName = player.DisplayName or player.Name
 
 local window = library:AddWindow("Zafiro HUB paid by SN | - Bienvenido " .. displayName, {
-    main_color = Color3.fromRGB(255, 212, 42),
+    main_color = Color3.fromRGB(193, 18, 31),
     min_size = Vector2.new(650, 870),
     can_resize = false,
 })
